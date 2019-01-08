8397565edc3fd46745ebdd9e4b4b474e39b16698
Petrovic, Ivan <ivan.petrovic@moodys.com>
2019-01-07 17:30:43 -0800

    Added supertest.

---
+++ server/server.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./server.js').app;
+
+it('should return hello world response', (done) => {
+    request(app)
+        .get('/')
+        .expect(200)
+        .expect('Hello world!')
+        .end(done);
+});
